adc36af472e671bcd1cf1387613ed7188e714b8b
Mark Andrew <mark.andrew@de.bosch.com>
2016-01-28 11:20:11 +0100

    added port configurability via cloudfoundry environment

--- src/main/java/io/first/MyFirstVerticle.java
+++ src/main/java/io/first/MyFirstVerticle.java
@@ -2,18 +2,24 @@ package io.first;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
+import java.util.Map;
 
 public class MyFirstVerticle extends AbstractVerticle {
 
   @Override
   public void start(Future<Void> fut) {
+    int port = 8080;
+    String portString = System.getenv("PORT");
+    if (portString != null) {
+      port = Integer.parseInt(portString);
+    }
     vertx
         .createHttpServer()
         .requestHandler(r -> {
           r.response().end("<h1>Hello from my first " +
               "Vert.x 3 application</h1>");
         })
-        .listen(8080, result -> {
+        .listen(port, result -> {
           if (result.succeeded()) {
             fut.complete();
           } else {
